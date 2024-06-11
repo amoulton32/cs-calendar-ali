7597f23585a5dc42117862d8bc3105ba381b6b65
Ali Moulton <ali.moulton@publicissapient.com>
2024-06-11 19:38:06 -0400

    selected date displayed
    added functionality to change selected day
    TODO: add coloring to distinguish selected day and jump to today button

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  const currentMonth = new Date().toLocaleString('default', { month: 'long' });
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <p>Ali Moulton </p>
+      <p>{currentMonth} {currentYear}</p>
+      <p>Clubspot Assessment</p>
+    </footer>
+  );
+}
+
+export default Footer;
