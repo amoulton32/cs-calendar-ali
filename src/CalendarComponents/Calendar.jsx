f6c97167067978721b0199d247d9ea64a579d612
Ali Moulton <ali.moulton@publicissapient.com>
2024-06-11 15:45:31 -0400

    date prop passing
    date prop is now passed correctly through app
    TODO:
    next step is to fill calendar with numbers, 1 should start on correct day of week

--- src/CalendarComponents/Calendar.jsx
+++ src/CalendarComponents/Calendar.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import CalCell from './CalCell'
 import './Calendar.css'
 
 const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-function Calendar() {
+
+
+function Calendar(props) {
+  // date passed in via props.value
+  console.log(props.value)
   return (
-    <>
+ 
       <div className="calendar-container">
         <h1>Clubspot Calendar</h1>       
         <div className='grid'>
@@ -16,12 +21,20 @@ function Calendar() {
 
             {/* DAYS TITLES */}
             {weekDays.map((day) => {
-                return <CalCell className="day-titles">{day}</CalCell>;
+                return <CalCell key={day} className="day-titles">{day}</CalCell>;
             })}
+
+            {/* number cells */}
+            {/* Render calendar cells for days */}
+            {Array(35).fill(null).map((_, index) => (
+                <CalCell key={index} date={new Date(2024, 1, index + 1)} />
+            ))}
+
+
         </div> 
       </div>
 
-    </>
+    
   )
 }
 
